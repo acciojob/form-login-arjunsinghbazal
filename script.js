54441b04c0ddadc87b2e387fbf8d4a5a89d33a0f
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-04-16 15:42:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function getFormvalue() {
-    //Write your code here
-
+  const firstName = document.getElementsByName("fname")[0].value;
+  const lastName = document.getElementsByName("lname")[0].value;
+  alert(firstName+" "+lastName);
 }
